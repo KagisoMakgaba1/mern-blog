da0d4c6d0136e16af1ae9c878db2cc6dd0c47182
Kagiso Makgaba <kagisomakgaba@outlook.com>
2024-11-26 14:56:02 +0200

    Add Google OAuth functionality

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "mern-blog-f5503.firebaseapp.com",
+  projectId: "mern-blog-f5503",
+  storageBucket: "mern-blog-f5503.firebasestorage.app",
+  messagingSenderId: "376323326307",
+  appId: "1:376323326307:web:a607fed682421c2b0fd356",
+};
+
+// Initialize Firebase
+ export const app = initializeApp(firebaseConfig);
