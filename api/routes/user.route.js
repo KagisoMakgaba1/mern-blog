fdbefc2dd686bb667563de6cb364f05b3426b5e0
Kagiso Makgaba <kagisomakgaba@outlook.com>
2025-01-08 14:55:50 +0200

    Show the comments of a post

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,12 +1,14 @@
 import express from "express";
 import {
   deleteUser,
+  getUser,
   getUsers,
   signout,
   test,
   updateUser,
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
+import { get } from "mongoose";
 
 const router = express.Router();
 
@@ -15,6 +17,7 @@ router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
 router.post("/signout", signout);
 router.get('/getusers', verifyToken, getUsers);
+router.get('/:userId', getUser);
 
 
 export default router;
